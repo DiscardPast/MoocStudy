6fec8bf873947c4a9b446a4a5cc953ef4e338ed3
DiscardPast <DiscardPast@yeah.net>
2017-07-17 07:25:33 +0800

    use ide write java

---
+++ JavaStudy/使用IDE编写Java程序/HelloWorld/src/com/discardpast/helloworld/HelloWorld.java
@@ -0,0 +1,11 @@
+package com.discardpast.helloworld;
+
+/**
+ * Created by discardpast on 17-7-17.
+ */
+public class HelloWorld {
+    public static void main(String[] args)
+    {
+        System.out.println("HelloWorld!");
+    }
+}
