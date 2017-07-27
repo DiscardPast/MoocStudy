5377f3b5768c54fd1e7a3e2285b8ab496f7654b8
DiscardPast <DiscardPast@yeah.net>
2017-07-27 22:42:10 +0800

    完善Hibernate之单表映射

---
+++ JavaStudy/框架与工具/Hibernate/Hibernate之单表映射/HibernateOneToManyMapping/src/com/discardpast/chapter_three_CRUDTest/SingleTableCRUD.java
@@ -0,0 +1,13 @@
+package com.discardpast.chapter_three_CRUDTest;
+
+/**
+ * 单表CRUD操作实例
+ * @author discardpast
+ *	保存一个对象:session.save()
+ *	修改一个对象:session.update()
+ *	删除一个对象:session.delete
+ *	查询单个记录:get()/load()
+ */
+public class SingleTableCRUD {
+
+}
